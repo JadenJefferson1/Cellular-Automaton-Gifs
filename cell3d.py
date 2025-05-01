ec59b9a8a8e5647feedae1f4b518cabfa6791dda
Jaden Jefferson <jadenjefferson18@gmail.com>
2025-05-01 12:21:05 -0400

    File name change and removal of unused libraries

--- cell3d.py
+++ cell3d.py
@@ -1,14 +1,11 @@
-import cellpylib as cpl
-
 import cellpylib3d
-
 import numpy as np
+
 # empty 3d grid
 grid = cellpylib3d.init_simple3d(15, 15, 15, val=0) # init empty 3d grid
 
 # oscilating shape from donut
 grid[:, [3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7], 4, [4, 5, 6, 3, 7, 3, 7, 3, 7, 4, 5, 6]] = 1
-#grid[:, [1], 4, [4, 5, 6, 6]] = 1
 
 # run using GOL ruleset for N timesteps
 cellular_automaton = cellpylib3d.evolve3d(grid, timesteps=100, neighbourhood='Moore', apply_rule=cellpylib3d.game_of_life_rule_3d)
